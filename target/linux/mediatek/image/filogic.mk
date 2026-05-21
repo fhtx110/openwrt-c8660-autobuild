851c422f601ea997d9ca740a26488f8df2e8bf91
fhtx110 <70973434+fhtx110@users.noreply.github.com>
2026-05-22 01:29:40 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2,10 +2,17 @@ define Device/nradio_c8660
   DEVICE_VENDOR := NRadio
   DEVICE_MODEL := C8-660
   DEVICE_DTS := mt7981-nradio-c8660
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := nradio,c8660
-  IMAGE_SIZE := 128m
-  IMAGES += sysupgrade.bin
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 kmod-mt7915e kmod-mt7981-firmware \
+	kmod-usb-net-qmi-wwan kmod-usb-serial-option uqmi \
+	mt7981-wo-firmware automount
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += nradio_c8660
